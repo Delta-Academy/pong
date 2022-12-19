a3c83cd1792e6674fd12e7619bce17db9a5dfc04
neuromantic99 <jrowlandm@gmail.com>
2022-12-19 10:19:28 +0000

    fixed check_submission

--- delta_pong/check_submission.py
+++ delta_pong/check_submission.py
@@ -1,21 +1,21 @@
 from pathlib import Path
 
-import delta_utils.check_submission as checker
-from torch import nn
+from delta_utils import get_discrete_choose_move_out_checker
+from delta_utils.check_submission import check_submission as _check_submission
 
 from game_mechanics import PongEnv, load_network
 
 
 def check_submission(team_name: str) -> None:
-    example_state, _, _, _ = PongEnv().reset()
-    expected_choose_move_return_type = int
-    expected_pkl_output_type = nn.Module
+    example_state, _, _, _ = PongEnv(lambda x: x).reset()
+    user_pkl_file = load_network(team_name)
 
-    return checker.check_submission(
-        example_state=example_state,
-        expected_choose_move_return_type=expected_choose_move_return_type,
-        expected_pkl_type=expected_pkl_output_type,
-        pkl_file=load_network(team_name),
-        pkl_checker_function=lambda x: x,
+    possible_outputs = [-1, 0, 1]
+
+    _check_submission(
+        example_choose_move_input={"state": example_state, "neural_network": user_pkl_file},
+        check_choose_move_output=get_discrete_choose_move_out_checker(
+            possible_outputs=possible_outputs,
+        ),
         current_folder=Path(__file__).parent.resolve(),
     )
